3af915467372b7ade8792039f4754054eb10ba0c
chenyn22577 <chenyn22577@hundsun.com>
2020-01-03 17:21:25 +0800

    add:搭建项目目录

---
+++ TestWs/TestWs/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace TestWs
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.WsMsg = new System.Windows.Forms.LinkLabel();
+            this.SuspendLayout();
+            // 
+            // WsMsg
+            // 
+            this.WsMsg.AutoSize = true;
+            this.WsMsg.Location = new System.Drawing.Point(54, 44);
+            this.WsMsg.Name = "WsMsg";
+            this.WsMsg.Size = new System.Drawing.Size(35, 12);
+            this.WsMsg.TabIndex = 0;
+            this.WsMsg.TabStop = true;
+            this.WsMsg.Text = "WsMsg";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.WsMsg);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.LinkLabel WsMsg;
+    }
+}
+
---
+++ TestWs/TestWs/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Fleck;
+
+namespace TestWs
+{
+    static class Program
+    {
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+            FleckLog.Level = LogLevel.Debug;
+            var allSockets = new List<IWebSocketConnection>();
+            var server = new WebSocketServer("ws://127.0.0.1:7181");
+            server.Start(socket =>
+            {
+                socket.OnOpen = () =>
+                {
+                    Console.WriteLine("Open!");
+                    allSockets.Add(socket);
+                };
+                socket.OnClose = () =>
+                {
+                    Console.WriteLine("Close!");
+                    allSockets.Remove(socket);
+                };
+                socket.OnMessage = message =>
+                {
+                    Console.WriteLine(message);
+                    allSockets.ToList().ForEach(s => s.Send("Echo: " + message));
+               };
+            });
+            var input = Console.ReadLine();
+            while (input != "exit")
+            {
+                foreach (var socket in allSockets.ToList())
+                {
+                    if (input != null) {
+                        socket.Send(input+"输出");
+                    };
+                }
+                input = Console.ReadLine();
+             }
+        }
+    }
+
+}
